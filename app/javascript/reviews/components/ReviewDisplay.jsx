5a4f301e1f5903ceb490b54e1cb7a1806bf3a298
ellerynz <hello@ellerynz.com>
2017-09-28 16:05:02 +1300

    Add form to add new reviews

--- app/javascript/reviews/components/ReviewDisplay.jsx
+++ app/javascript/reviews/components/ReviewDisplay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ReviewDisplay extends React.Component {
 
@@ -8,10 +9,11 @@ class ReviewDisplay extends React.Component {
 
   render() {
     return (
-      <div>
+      <li>
+        <hr />
         <p>{this.props.name}</p>
         <p>{this.props.rating}</p>
-      </div>
+      </li>
     );
   }
 
@@ -20,6 +22,11 @@ class ReviewDisplay extends React.Component {
 ReviewDisplay.defaultProps = {
   name: 'Blank',
   rating: 0
+}
+
+ReviewDisplay.propTypes = {
+  name: PropTypes.string,
+  rating: PropTypes.number
 }
 
 export default ReviewDisplay
--- app/javascript/reviews/components/ReviewList.jsx
+++ app/javascript/reviews/components/ReviewList.jsx
@@ -1,35 +1,53 @@
 import React from  'react';
 import { Link } from 'react-router-dom';
 import ReviewDisplay from './ReviewDisplay';
+import AddReviewForm from './AddReviewForm';
 
 class ReviewList extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [
+        { id: 1, name: 'foo', rating: 3 },
+        { id: 2, name: 'bar', rating: 2 },
+        { id: 3, name: 'baz', rating: 1 }
+      ]
+    };
+  }
+
+  addReview(name, rating) {
+    const lastId = this.state.reviews.length;
+    const reviews = [
+      ...this.state.reviews,
+      {
+        id: lastId + 1,
+        name: name,
+        rating: parseInt(rating, 10)
+      }
+    ]
+    this.setState({reviews});
   }
 
   render() {
-    const reviews = [
-      { id: 1, name: 'foo', rating: 3 },
-      { id: 2, name: 'bar', rating: 2 },
-      { id: 3, name: 'baz', rating: 1 }
-    ];
-
     return (
-      <ul>
-        {
-          reviews.map(review =>
-            <ReviewDisplay
-              key={review.id}
-              name={review.name}
-              rating={review.rating}
-            />
-          )
-        }
-      </ul>
+      <div>
+        <AddReviewForm onSubmit={this.addReview.bind(this)} />
+        <ul>
+          {
+            this.state.reviews.map(review =>
+              <ReviewDisplay
+                key={review.id}
+                name={review.name}
+                rating={review.rating}
+              />
+            )
+          }
+          <hr />
+        </ul>
+      </div>
     );
   }
-
 }
 
 export default ReviewList
